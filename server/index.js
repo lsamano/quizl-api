3c6817b804221cdbb9f90e2bb0cb9c3979a7e4ea
lsamano <leizl.samano@gmail.com>
2020-08-22 17:41:31 -0400

    added cors

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
-const Quiz = require('./models/quiz');
-require('dotenv').config();
+const cors = require('cors');
 
+require('dotenv').config();
 const API_PORT = process.env.PORT;
 const DB_URI = process.env.DB_URI;
+
+const routes = require('./routes');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+
+app.use('/quizzes', routes)
 
 mongoose.connect(DB_URI, {
   useUnifiedTopology: true,
