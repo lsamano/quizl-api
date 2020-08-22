f80e489a9e17d854d33bd68425528c7c3b78939d
lsamano <leizl.samano@gmail.com>
2020-08-21 22:19:02 -0400

    connected to mongodb, added morgan and env variables

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+require('dotenv').config();
 
-const API_PORT = 3000;
+const API_PORT = process.env.PORT;
+const DB_URI = process.env.DB_URI;
 
 const app = express();
+
+app.use(logger('dev'));
+
+mongoose.connect(DB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
 
 app.listen(API_PORT, () => console.log(`Listening on Port ${API_PORT}...`));
